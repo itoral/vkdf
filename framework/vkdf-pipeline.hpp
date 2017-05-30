97b0551f95a7b33b8aa6705401a7f51f39a91e5b
Alejandro Piñeiro <apinheiro@igalia.com>
2017-05-30 15:41:44 +0200

    framework: add vkdf_create_basic_pipeline
    Similar to vkdf_create_gfx_pipeline but even more basic, useful when you
    want to define a basic pipeline with just one shader stage, and no need
    to customize some options (example: compute shader).
    
    Compared with vkdf_create_gfx_pipeline:
      * Just one shader stage
      * No dynamics
      * No depth test
      * No cull mode

--- framework/vkdf-pipeline.hpp
+++ framework/vkdf-pipeline.hpp
@@ -2,6 +2,19 @@
 #define __VKDF_PIPELINE_H__
 
 VkPipeline
+vkdf_create_basic_pipeline(VkdfContext *ctx,
+                           VkPipelineCache *pipeline_cache,
+                           uint32_t num_vi_bindings,
+                           VkVertexInputBindingDescription *vi_bindings,
+                           uint32_t num_vi_attribs,
+                           VkVertexInputAttributeDescription *vi_attribs,
+                           VkRenderPass render_pass,
+                           VkPipelineLayout pipeline_layout,
+                           VkPrimitiveTopology primitive,
+                           VkShaderModule module,
+                           VkShaderStageFlagBits stage);
+
+VkPipeline
 vkdf_create_gfx_pipeline(VkdfContext *ctx,
                          VkPipelineCache *cache,
                          uint32_t num_vi_bindings,
