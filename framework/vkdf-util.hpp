4b3a1a5a7fead593ebdfcccd7589c99ee1827b29
Iago Toral Quiroga <itoral@igalia.com>
2017-07-23 12:37:52 +0200

    framework: add some vector utils

--- framework/vkdf-util.hpp
+++ framework/vkdf-util.hpp
@@ -1,6 +1,18 @@
 #ifndef __VKDF_UTIL_H__
 #define __VKDF_UTIL_H__
 
+inline glm::vec3
+vec3(glm::vec4 v)
+{
+   return glm::vec3(v.x, v.y, v.z);
+}
+
+inline glm::vec4
+vec4(glm::vec3 v, float w)
+{
+   return glm::vec4(v.x, v.y, v.z, w);
+}
+
 static inline float
 vkdf_vec3_module(glm::vec3 p, int xaxis, int yaxis, int zaxis)
 {
