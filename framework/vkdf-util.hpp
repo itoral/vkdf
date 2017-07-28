ba6a7ff74726154b15cf65b168ebb2e2aa56530a
Iago Toral Quiroga <itoral@igalia.com>
2017-07-28 15:04:36 +0200

    framework: add utils to compute the dot and cross products

--- framework/vkdf-util.hpp
+++ framework/vkdf-util.hpp
@@ -35,6 +35,22 @@ vkdf_vec3_normalize(glm::vec3 *p)
    }
 }
 
+static inline float
+vkdf_vec3_dot(glm::vec3 v1, glm::vec3 v2)
+{
+   return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+static inline glm::vec3
+vkdf_vec3_cross(glm::vec3 v1, glm::vec3 v2)
+{
+   glm::vec3 r;
+   r.x = v1.y * v2.z - v1.z * v2.y;
+   r.y = v1.z * v2.x - v1.x * v2.z;
+   r.z = v1.x * v2.y - v1.y * v2.x;
+   return r;
+}
+
 glm::vec3
 vkdf_compute_view_rotation(glm::vec3 origin, glm::vec3 target);
 
