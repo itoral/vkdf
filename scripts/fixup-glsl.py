a30b153f1624ed0f2e03b5b002478156f5e4beb0
Iago Toral Quiroga <itoral@igalia.com>
2017-08-14 20:30:14 +0200

    scripts: add a script to include GLSL snippets into GLSL shaders

---
+++ scripts/fixup-glsl.py
@@ -0,0 +1,26 @@
+import sys
+import re
+
+def processline(line, regexp):
+   m = regexp.match(line)
+   inc_lines = [line]
+   if m:
+      s = m.span()
+      inc_filename = line[s[0]+8:s[1]-1]
+      inc_lines = open(inc_filename).readlines()
+   return inc_lines
+
+in_filename = sys.argv[1]
+out_filename = sys.argv[2]
+regexp = re.compile('INCLUDE(.*)')
+new_lines = []
+
+f = open(in_filename);
+for line in f:
+   new_lines += processline(line, regexp)
+f.close()
+
+f = open(out_filename, 'w')
+for line in new_lines:
+   f.write(line)
+f.close()
