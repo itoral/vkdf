ea763afa91812df2796bf31709709edacd1db53d
Iago Toral Quiroga <itoral@igalia.com>
2017-09-05 15:54:59 +0200

    demos/scenelight: reduce register pressure in fragment shader
    We were dropping to SIMD8 execution in Intel due to excessive
    register pressure. Simplify a bit the code so we can do SIMD16
    for a significant performance boost.

--- demos/scenelight/gencolor.glsl
+++ demos/scenelight/gencolor.glsl
@@ -2,36 +2,28 @@
    // samplers to simplify this. Unfortunately, Mesa fails to compile such
    // shader at present.
    out_color = vec4(0.0, 0.0, 0.0, 1.0);
-   for (int i = 0; i < NUM_LIGHTS; i++) {
-      Light light = L.lights[i];
 
-      LightColor color;
-      if (light.casts_shadows) {
-         vec4 light_space_pos = in_light_space_pos[i];
-         uint shadow_map_size = SMD.data[i].shadow_map_size;
-         uint pfc_size = SMD.data[i].pfc_kernel_size;
-         if (i == 0) {
-            color = compute_lighting(light,
-                                     in_world_pos.xyz,
-                                     in_normal, in_view_dir,
-                                     mat,
-                                     bool(in_receives_shadows),
-                                     light_space_pos, shadow_map0,
-                                     shadow_map_size, pfc_size);
-         } else {
-            color = compute_lighting(light,
-                                     in_world_pos.xyz,
-                                     in_normal, in_view_dir,
-                                     mat,
-                                     bool(in_receives_shadows),
-                                     light_space_pos, shadow_map1,
-                                     shadow_map_size, pfc_size);
-         }
-      } else {
-            color = compute_lighting(L.lights[i],
-                                     in_world_pos.xyz,
-                                     in_normal, in_view_dir,
-                                     mat);
-      }
-      out_color.xyz += color.diffuse + color.ambient + color.specular;
-   }
+   LightColor color;
+   color = compute_lighting(L.lights[0],
+                            in_world_pos.xyz,
+                            in_normal, in_view_dir,
+                            mat,
+                            bool(in_receives_shadows),
+                            in_light_space_pos[0],
+                            shadow_map0,
+                            SMD.data[0].shadow_map_size,
+                            SMD.data[0].pfc_kernel_size);
+
+   out_color.xyz += color.diffuse + color.ambient + color.specular;
+
+   color = compute_lighting(L.lights[1],
+                            in_world_pos.xyz,
+                            in_normal, in_view_dir,
+                            mat,
+                            bool(in_receives_shadows),
+                            in_light_space_pos[1],
+                            shadow_map1,
+                            SMD.data[1].shadow_map_size,
+                            SMD.data[1].pfc_kernel_size);
+
+   out_color.xyz += color.diffuse + color.ambient + color.specular;
