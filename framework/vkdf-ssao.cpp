e18743c4c7e241f1e9f12377da943338f67c452e
Iago Toral Quiroga <itoral@igalia.com>
2018-01-27 18:14:29 +0100

    framework/ssao: add a helper to generate the SSAO noise image

--- framework/vkdf-ssao.cpp
+++ framework/vkdf-ssao.cpp
@@ -56,3 +56,21 @@ vkdf_ssao_gen_noise_samples(uint32_t num_samples,
       samples->push_back(sample);
    }
 }
+
+void
+vkdf_ssao_gen_noise_image(VkdfContext *ctx,
+                          VkCommandPool pool,
+                          uint32_t width,
+                          uint32_t height,
+                          const std::vector<glm::vec3> *samples,
+                          VkdfImage *image)
+{
+   assert(width * height <= samples->size());
+
+   vkdf_create_image_from_data(ctx, pool,
+                               width, height,
+                               VK_FORMAT_R16G16B16_SFLOAT,
+                               false,
+                               &(*samples)[0],
+                               image);
+}
--- framework/vkdf-ssao.hpp
+++ framework/vkdf-ssao.hpp
@@ -9,4 +9,12 @@ void
 vkdf_ssao_gen_noise_samples(uint32_t num_samples,
                             std::vector<glm::vec3> *samples);
 
+void
+vkdf_ssao_gen_noise_image(VkdfContext *ctx,
+                          VkCommandPool pool,
+                          uint32_t width,
+                          uint32_t height,
+                          const std::vector<glm::vec3> *samples,
+                          VkdfImage *image);
+
 #endif
