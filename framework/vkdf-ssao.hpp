530397d0133378b9444259acac0ec07114133967
Iago Toral Quiroga <itoral@igalia.com>
2018-01-28 12:51:34 +0100

    framework/ssao: add a helper to create the SSAO texture sampler

--- framework/vkdf-ssao.hpp
+++ framework/vkdf-ssao.hpp
@@ -27,4 +27,14 @@ vkdf_ssao_create_noise_sampler(VkdfContext *ctx)
                               0.0f);
 }
 
+inline VkSampler
+vkdf_ssao_create_ssao_sampler(VkdfContext *ctx, VkFilter filter)
+{
+   return vkdf_create_sampler(ctx,
+                              VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE,
+                              filter,
+                              VK_SAMPLER_MIPMAP_MODE_NEAREST,
+                              0.0f);
+}
+
 #endif
