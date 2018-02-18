2786b66241047781e6f2ca5117fa5be15a3f6a34
Iago Toral Quiroga <itoral@igalia.com>
2018-02-18 13:06:09 +0100

    framework/scene: add sampled flag to color framebuffers
    So they can be sampled from postprocessing passes.

--- framework/vkdf-scene.cpp
+++ framework/vkdf-scene.cpp
@@ -124,8 +124,10 @@ create_color_framebuffer_image(VkdfScene *s)
                             1,
                             VK_IMAGE_TYPE_2D,
                             VK_FORMAT_R8G8B8A8_UNORM,
-                            VK_FORMAT_FEATURE_COLOR_ATTACHMENT_BIT,
+                            VK_FORMAT_FEATURE_COLOR_ATTACHMENT_BIT |
+                              VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT,
                             VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT |
+                               VK_IMAGE_USAGE_SAMPLED_BIT |
                                VK_IMAGE_USAGE_TRANSFER_SRC_BIT,
                             VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
                             VK_IMAGE_ASPECT_COLOR_BIT,
