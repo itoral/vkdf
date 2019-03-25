43e0e76b59ff4a3dffb7d8fc3f9e80629549afd6
Iago Toral Quiroga <itoral@igalia.com>
2019-03-25 08:56:09 +0100

    demos/sponza: fix broken link to texture pack

--- demos/sponza/download-textures.sh
+++ demos/sponza/download-textures.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
+
+TEXTURES_URL=http://people.igalia.com/itoral/sponza_textures.tgz
+
 echo "Download and unpack textures for the Sponza model. This needs the 'wget' and 'unrar' tools.\n"
 echo "Downloading Sponza texture pack from Crytek site. This can take some time..."
-wget http://www.crytek.com/download/sponza_textures.rar -O sponza_textures.rar
+
+
+wget $TEXTURES_URL -O sponza_textures.tgz
+
 if [ $? -eq 0 ]; then
    echo "Unpacking textures..."
-   unrar x sponza_textures.rar
+   umask 022
+   mkdir -p textures
+   tar zxf sponza_textures.tgz -C textures/
 else
    echo "Failed to download!"
 fi
